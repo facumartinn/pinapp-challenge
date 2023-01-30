b4a895ba3892cbd40abca77dea656518ca235683
unknown <you@example.com>
2023-01-30 15:32:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Heading, Box } from '@chakra-ui/react';
+import { NewClientForm } from './components/new-client-form/index'
+import { appStyles } from './styles';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={4} sx={appStyles.body}>
+      <Box sx={appStyles.container}>
+        <Heading>PinApp</Heading>
+        <NewClientForm />
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/styles/index.js
@@ -0,0 +1,12 @@
+export const appStyles = {
+    body: {
+        display: "flex", 
+        width: "100%", 
+        justifyContent: "center"
+    },
+    container: {
+        width: "500px", 
+        display: "flex", 
+        flexDirection: "column"
+    }
+}
